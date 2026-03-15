a9d7737b97a805406ad5c0d87e9e33e957eef1d8
test5574663-coder <test5574663@gmail.com>
2026-03-15 08:05:59 +0700

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,1 +1,28 @@
 
+const { REST, Routes, SlashCommandBuilder } = require("discord.js");
+const TOKEN = process.env.TOKEN;
+
+const CLIENT_ID = "1472161934609744106";
+const GUILD_ID = "1466476014908473550";
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName("onduty")
+    .setDescription("Bắt đầu trực")
+    .addStringOption(o =>
+      o.setName("bienso")
+        .setDescription("Biển số")
+        .setRequired(false)
+    ),
+
+  new SlashCommandBuilder()
+    .setName("offduty")
+    .setDescription("Kết thúc trực"),
+
+  new SlashCommandBuilder()
+    .setName("resetduty")
+    .setDescription("Reset giờ trực")
+].map(c => c.toJSON());
+
+const rest = new REST({ version: "10" }).setToken(TOKEN);
+rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: [] });
